4da46569a9c2b6614cd7f9dfe3807a0e006ebc94
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-08-03 13:51:31 -0600

    Dispatch Object Created

--- src/components/Synthesizer.js
+++ src/components/Synthesizer.js
@@ -7,13 +7,7 @@ let OSCILLATOR_1 = context.createOscillator();
 
 const GainNode = context.createGain();
 GainNode.gain.value = 0;
-<<<<<<< HEAD
 GainNode.connect(context.destination)
-=======
-GainNode.connect(context.destination);
->>>>>>> 2512bb05b107a00e1d70329407f13cbd4d181e47
-
-
 
 function playSound() {
 	OSCILLATOR_1.frequency.value = 440;
@@ -21,27 +15,20 @@ function playSound() {
 	OSCILLATOR_1.start();
 }
 
-// playSound();
-
+playSound();
 
 class Synthesizer extends Component {
 	constructor(props) {
 		super(props);
 
-<<<<<<< HEAD
     this.state = {
       patch: {
         oscillator: [{}, {}, {}],
 
       }
     }
-
     this.receiveDispatch = this.receiveDispatch.bind(this);
   }
-=======
-		this.receiveDispatch = this.receiveDispatch.bind(this);
-	}
->>>>>>> 2512bb05b107a00e1d70329407f13cbd4d181e47
 
 	receiveDispatch(type, property, value, id) {
 		dispatches[type][property](value);
@@ -59,31 +46,218 @@ class Synthesizer extends Component {
 export default Synthesizer;
 
 const dispatches = {
-<<<<<<< HEAD
   oscillator: {
-    waveform: function(input) {
-      console.log(input);
-      OSCILLATOR_1.type = input
-    },
-    gain: function(value) {
-      console.log(value);
-      GainNode.gain.value = value;
-    },
-    detune: function(value) {
-
-    }
+      waveform: function(input) {
+        console.log("Oscillator waveform: ", input);
+        OSCILLATOR_1.type = input
+      },
+      gain: function(value) {
+        console.log("OSC Gain: ",value);
+        GainNode.gain.value = value;
+      },
+      detune: function(value) {
+        console.log("OSC Detune: ", value);
+      }
+  },
+  adsr: {
+      attack: function(value) {
+        console.log("Master Attack: ", value);
+      },
+      decay: function(value) {
+        console.log("Master Decay: ", value);
+      },
+      sustain: function(value) {
+        console.log("Master Sustain: ", value);
+      },
+      release: function(value) {
+        console.log("Master Release: ", value);
+      }
+  },
+  filter: {
+      type: function(input) {
+        console.log("filter type: ", input);
+      },
+      frequency: function(value) {
+        console.log("frequency: ", value);
+      },
+      Q: function(value) {
+        console.log("Q: ", value);
+      },
+      gain: function(value) {
+        console.log("gain: ", value);
+      },
+      attack: function(value) {
+        console.log("Filter Attack: ", value);
+      },
+      decay: function(value) {
+        console.log("Filter Decay: ", value);
+      },
+      sustain: function(value) {
+        console.log("Filter Sustain: ", value);
+      },
+      release: function(value) {
+        console.log("Filter Release: ", value);
+      }
+  },
+  lfo: {
+      type: function(input) {
+        console.log("LFO type: ", input);
+      },
+      frequency: function(value) {
+        console.log("LFO frequency: ", value);
+      },
+      modGain: function(value) {
+        console.log("Mod Gain: ", value);
+      }
+  },
+  compressor: {
+      threshold: function(value){
+        console.log("comp threshold: ", value);
+      },
+      makeUpGain: function(value) {
+        console.log("comp makeUpGain: ", value);
+      },
+      attack: function(value) {
+        console.log("comp attack: ", value);
+      },
+      release: function(value) {
+        console.log("comp release: ", value);
+      },
+      ratio: function(value) {
+        console.log("comp ratio: ", value);
+      },
+      knee: function(value) {
+        console.log("comp knee: ", value);
+      },
+      autoMakeUp: function(boolean) {
+        console.log("automakeup compressor: ", boolean);
+      },
+      bypass: function(value) {
+        console.log("compressor bypass: ", value);
+      }
+  },
+  convolver: {
+      highcut: function(value) {
+        console.log("HC verb: ", value);
+      },
+      lowcut: function(value) {
+        console.log("LC verb: ", value);
+      },
+      dryLevel: function(value) {
+        console.log("dryLevel verb: ", value);
+      },
+      wetLevel: function(value) {
+        console.log("wetLevel verb: ", value);
+      },
+      level: function(value) {
+        console.log("Level verb: ", value);
+      },
+      bypass: function(value) {
+        console.log("bypass verb: ", value);
+      },
+      order: function(value) {
+        console.log("FX Order verb: ", value);
+      }
+  },
+  ping_pong: {
+      feedback: function(value) {
+        console.log("Ping Pong Feedback: ", value);
+      },
+      wet: function(value) {
+        console.log("Ping Pong wet: ", value);
+      },
+      dry: function(value) {
+        console.log("Ping Pong Feedback: ", value);
+      },
+      delay_left: function(value) {
+        console.log("Ping Pong Feedback: ", value);
+      },
+      delay_right: function(value) {
+        console.log("Ping Pong Feedback: ", value);
+      }
+  },
+  phaser: {
+      rate: function(value) {
+        console.log("Phaser rate: ", value);
+      },
+      depth: function(value) {
+        console.log("Phaser depth: ", value);
+      },
+      feedback: function(value) {
+        console.log("Phaser feedback: ", value);
+      },
+      stereo_phase: function(value) {
+        console.log("Phaser stereo_phase: ", value);
+      },
+      BMF: function(value) {
+        console.log("Phaser BMF: ", value);
+      }
+  },
+  overdrive: {
+      drive: function(value) {
+        console.log("overdrive drive: ", value);
+      },
+      output_gain: function(value) {
+        console.log("overdrive output_gain: ", value);
+      },
+      curve_amount: function(value) {
+        console.log("overdrive curve_amount: ", value);
+      },
+      algorithm_index: function(value) {
+        console.log("overdrive algorithm_index: ", value);
+      }
+  },
+  moog_fliter: {
+      buffer: function(value) {
+        console.log("Moog buffer: ", value);
+      },
+      cuttoff: function(value) {
+        console.log("Moog cutoff: ", value);
+      },
+      res: function(value) {
+        console.log("Moog res: ", value);
+      }
+  },
+  chorus: {
+      feedback: function(value) {
+        console.log("Chorus feedback: ", value);
+      },
+      delay: function(value) {
+        console.log("Chorus delay: ", value);
+      },
+      depth: function(value) {
+        console.log("Chorus depth: ", value);
+      },
+      rate: function(value) {
+        console.log("Chorus rate: ", value);
+      }
+  },
+  bitcrusher: {
+      bits: function(value) {
+        console.log("Bit bits: ", value);
+      },
+      buffer: function(value) {
+        console.log("Bits buffer: ", value);
+      },
+      norm_freq: function(value) {
+        console.log("Bits norm_freq: ", value);
+      }
+  },
+  delay: {
+      time: function(value) {
+        console.log("Delay time: ", value);
+      },
+      feedback: function(value) {
+        console.log("Delay feedback: ", value);
+      },
+      cuttoff: function(value) {
+        console.log("Delay cuttoff: ", value);
+      },
+      dry: function(value) {
+        console.log("Delay dry: ", value);
+      },
+      wet: function(value) {
+        console.log("Delay wet: ", value);
+      }
   }
 }
-=======
-	oscillator: {
-		waveform: function(input) {
-			console.log(input);
-			OSCILLATOR_1.type = input;
-		},
-		gain: function(value) {
-			console.log(value);
-			GainNode.gain.value = value;
-		}
-	}
-};
->>>>>>> 2512bb05b107a00e1d70329407f13cbd4d181e47
