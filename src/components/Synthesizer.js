9451550d699da5be905031b3fd69f90966366261
Ross Kiser <kiserr10@gmail.com>
2017-08-03 11:55:11 -0600

    layout component started with three osc

--- src/components/Synthesizer.js
+++ src/components/Synthesizer.js
@@ -1,56 +1,56 @@
 import React, { Component } from 'react';
-import Oscillator from './Oscillator';
+import Layout from './Layout';
 
 const context = new AudioContext();
 
 let OSCILLATOR_1 = context.createOscillator();
 
 const GainNode = context.createGain();
-  GainNode.gain.value = 0;
-  GainNode.connect(context.destination)
+GainNode.gain.value = 0;
+GainNode.connect(context.destination);
 
 
 
 function playSound() {
-  OSCILLATOR_1.frequency.value = 440;
-  OSCILLATOR_1.connect(GainNode);
-  OSCILLATOR_1.start();
+	OSCILLATOR_1.frequency.value = 440;
+	OSCILLATOR_1.connect(GainNode);
+	OSCILLATOR_1.start();
 }
 
-
+// playSound();
 
 
 class Synthesizer extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.receiveDispatch = this.receiveDispatch.bind(this);
-  }
+		this.receiveDispatch = this.receiveDispatch.bind(this);
+	}
 
-  receiveDispatch(type, property, value, id) {
-    dispatches[type][property](value);
-  }
+	receiveDispatch(type, property, value, id) {
+		dispatches[type][property](value);
+	}
 
-  render() {
-    return (
+	render() {
+		return (
       <div>
-        <Oscillator sendDispatch={this.receiveDispatch}/>
+        <Layout sendDispatch={this.receiveDispatch}/>
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default Synthesizer;
 
 const dispatches = {
-  oscillator: {
-    waveform: function(input) {
-      console.log(input);
-      OSCILLATOR_1.type = input
-    },
-    gain: function(value) {
-      console.log(value);
-      GainNode.gain.value = value;
-    }
-  }
-}
+	oscillator: {
+		waveform: function(input) {
+			console.log(input);
+			OSCILLATOR_1.type = input;
+		},
+		gain: function(value) {
+			console.log(value);
+			GainNode.gain.value = value;
+		}
+	}
+};
