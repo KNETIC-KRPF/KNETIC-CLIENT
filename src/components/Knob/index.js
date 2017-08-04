f2b5df58e14fcf22c0e5b75788c45e31c6723a26
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-08-04 14:52:32 -0600

    Knob Working

--- src/components/Knob/index.js
+++ src/components/Knob/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import './Knob.css';
 
 const Knob = (props) => {
-  return (<input id="knob" type="range" min={props.min} max={props.max} step={props.step} data-degree-range="270" defaultValue="0" onChange={(event) => props.sendDispatch(props.type, props.property, event.target.value)} data-degree-offset="45"/>);
+  return (<input id="knob" type="range" min={props.min} max={props.max} step={props.step} data-degree-range="270" defaultValue="0"
+  onChange={(event) => props.sendDispatch(props.type, props.property, event.target.value)} data-degree-offset="45"/>);
 };
 
 export default Knob;
--- src/components/OscillatorKnob/index.js
+++ src/components/OscillatorKnob/index.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import KnobComp from 'react-canvas-knob';
 
-import './OscillatorKnob.css';
+class Knob extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+			value: 0
+		};
+		this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = (newValue) => {
+		this.props.sendDispatch(this.props.type, this.props.property, newValue, this.props.id);
+    this.setState({
+			value: newValue
+		});
+  };
 
-const Knob = (props) => {
-	return (
-    <input
-      type="range"
-      min={props.min}
-      max={props.max}
-      step={props.step}
-      data-degree-range="270"
-      defaultValue="0"
-      onChange={(event) => props.sendDispatch(props.type, props.property, event.target.value, props.id)}
-      data-degree-offset="45"/>
-	);
-};
+  handleChangeEnd(event) {
+  };
+
+  render() {
+    return (
+      <KnobComp
+        value={this.state.value}
+        onChange={this.handleChange}
+        onChangeEnd={this.handleChangeEnd}
+        thickness={0.2}
+        width={100}
+        height={100}
+        stopper={true}
+        angleArc={270}
+        angleOffset={-135}
+        disableMouseWheel={true}
+        fgColor="#94E4C2"
+        inputColor="#94E4C2"
+        min={this.props.min}
+        max={this.props.max}
+        step={this.props.step}
+      />
+    );
+  }
+}
 
 export default Knob;
