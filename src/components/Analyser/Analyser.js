6b5248330081bcb15d452fa642c9a3a4d548c62f
Alex Read <alexread730@gmail.com>
2017-08-06 15:09:49 -0600

    began analyser branch'

---
+++ src/components/Analyser/Analyser.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import KnobComp from 'react-canvas-knob';
+
+class Knob extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+			value: this.props.patchState
+		};
+		this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = (newValue) => {
+		this.props.sendDispatch(this.props.type, this.props.property, newValue, this.props.id);
+    this.setState({
+			value: newValue
+		});
+  };
+
+  handleChangeEnd(event) {
+  };
+
+  render() {
+    return (
+      <KnobComp
+        value={this.state.value}
+        onChange={this.handleChange}
+        onChangeEnd={this.handleChangeEnd}
+        thickness={0.5}
+        width={40}
+        height={40}
+        stopper={true}
+        angleArc={270}
+        angleOffset={-135}
+        disableMouseWheel={true}
+        displayInput={true}
+        fgColor="#94E4C2"
+        bgColor="#999797"
+        inputColor="#94E4C2"
+        min={this.props.min}
+        max={this.props.max}
+        step={this.props.step}
+      />
+    );
+  }
+}
+
+export default Knob;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -20,67 +20,67 @@ class Layout extends Component {
         <div className="container">
           <Header/>
           <div className="grid">
-          <div className="left-column grid-cell">
-            <label htmlFor="oscillator-one">Oscillator 1</label>
-            <Oscillator
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
-              id={1}
+            <div className="left-column grid-cell">
+              <label htmlFor="oscillator-one">Oscillator 1</label>
+              <Oscillator
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
+                id={1}
               />
-            <label htmlFor="oscillator-two">Oscillator 2</label>
-            <Oscillator
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
-              id={2}
+              <label htmlFor="oscillator-two">Oscillator 2</label>
+              <Oscillator
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
+                id={2}
               />
-            <label htmlFor="oscillator-three">Oscillator 3</label>
-            <Oscillator
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
-              id={3}
+              <label htmlFor="oscillator-three">Oscillator 3</label>
+              <Oscillator
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
+                id={3}
               />
-            <ADSR
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
+              <ADSR
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
               />
-          </div>
+            </div>
 
-          <div className="middle-column grid-cell">
-            <Delay
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
+            <div className="middle-column grid-cell">
+              <Delay
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
               />
-            <Reverb
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
+              <Reverb
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
               />
-            <PingPongDelay
-              sendDispatch={this.props.sendDispatch}
+              <PingPongDelay
+                sendDispatch={this.props.sendDispatch}
               />
-            <Phaser
-              sendDispatch={this.props.sendDispatch}
+              <Phaser
+                sendDispatch={this.props.sendDispatch}
               />
-            <Overdrive
-              sendDispatch={this.props.sendDispatch}
+              <Overdrive
+                sendDispatch={this.props.sendDispatch}
               />
-            <MoogFilter
-              sendDispatch={this.props.sendDispatch}
+              <MoogFilter
+                sendDispatch={this.props.sendDispatch}
               />
-            <Chorus
-              sendDispatch={this.props.sendDispatch}
+              <Chorus
+                sendDispatch={this.props.sendDispatch}
               />
-            <Bitcrusher
-              sendDispatch={this.props.sendDispatch}
+              <Bitcrusher
+                sendDispatch={this.props.sendDispatch}
               />
-          </div>
-          <div className="right-column grid-cell">
-            <Filter
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
+            </div>
+            <div className="right-column grid-cell">
+              <Filter
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
               />
-            <Compressor
-              patch={this.props.patch}
-              sendDispatch={this.props.sendDispatch}
+              <Compressor
+                patch={this.props.patch}
+                sendDispatch={this.props.sendDispatch}
               />
           </div>
         </div>
