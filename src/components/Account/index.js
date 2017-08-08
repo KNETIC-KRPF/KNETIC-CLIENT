540ddc93aec2264b31020ad7a8f45dda7ba473bc
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-08-08 12:27:55 -0600

    Style

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -13,7 +13,9 @@ const signupStyle = {
     backgroundColor: '#6c2505',
     backgroundSize: '40px 160px, 60px 29px, 27px 27px',
     backgroundImage: 'linear-gradient(94deg, rgba(91, 33, 5, 0.1) 0%, rgba(83, 29, 4, 0.32) 23%, rgba(74, 24, 3, 0.41) 47%, rgba(80, 27, 5, 0.44) 70%, rgba(81, 27, 5, 0.59) 74%, rgba(93, 33, 4, 0.2) 83%, rgba(115, 43, 3, 0.5) 100%), linear-gradient(90deg, #541c09 50%, transparent 50%), linear-gradient(90deg, #7a2e00 50%, #632401 50%)',
-    width: '400px'
+    width: '400px',
+    borderRadius: '5%',
+    zIndex: '1'
   }
 }
 
@@ -102,11 +104,11 @@ class Account extends Component {
 								name="password"
 								placeholder="password"/>
 							<br/>
-							<button className="btn modal" type="submit">Submit</button>
+							<button className="btn modal" type="submit">SUBMIT</button>
 						</form>
 						<button
 							className="btn modal"
-							onClick={this.closeModal}>Close</button>
+							onClick={this.closeModal}>CLOSE</button>
 					</div>
 				</Modal>
 
--- src/components/PatchCollection/index.js
+++ src/components/PatchCollection/index.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import './PatchCollection.css';
 
+import {ROOT_URL} from '../../RootURL';
+
 class PatchCollection extends Component {
+	constructor(props) {
+	  super(props);
+
+		this.state = {
+			patches: []
+		}
+	}
+
+	componentDidMount() {
+		fetch(`${ROOT_URL}/patches`)
+		.then(res => res.json())
+		.then(res => {
+			const newState = [...this.state.patches];
+			newState.concat(res)
+			this.setState({
+				patches: newState
+			});
+		})
+	}
+
+	renderOptions() {
+		this.state.patches.map((patch, i) => {
+			return <option value={}></option>
+		});
+	}
+
 	render() {
 		return(
 			<div>
