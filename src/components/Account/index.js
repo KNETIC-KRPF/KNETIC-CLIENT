90f569d6338679c95339e53e1b972ab2220b9ca1
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-08-08 10:57:23 -0600

    Signup Modal Added

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -9,7 +9,11 @@ const signupStyle = {
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#6c2505',
+    backgroundSize: '40px 160px, 60px 29px, 27px 27px',
+    backgroundImage: 'linear-gradient(94deg, rgba(91, 33, 5, 0.1) 0%, rgba(83, 29, 4, 0.32) 23%, rgba(74, 24, 3, 0.41) 47%, rgba(80, 27, 5, 0.44) 70%, rgba(81, 27, 5, 0.59) 74%, rgba(93, 33, 4, 0.2) 83%, rgba(115, 43, 3, 0.5) 100%), linear-gradient(90deg, #541c09 50%, transparent 50%), linear-gradient(90deg, #7a2e00 50%, #632401 50%)',
+    width: '400px'
   }
 }
 
@@ -54,13 +58,13 @@ class Account extends Component {
 				<form className="login" onSubmit={this.submitLogin}>
 
 					<input
-						className="username"
+						className="login-input"
 						type="text"
 						name="username"
 						placeholder="username"/>
 
 					<input
-						className="password"
+						className="login-input"
 						type="password"
 						name="password"
 						placeholder="password"/>
