bf6990ed912714249eb9ace93438a09fcf9fcd6b
Alex Read <alexread730@gmail.com>
2017-08-08 19:11:27 -0600

    merged

--- src/RootURL.js
+++ src/RootURL.js
@@ -1,1 +1,1 @@
-export const ROOT_URL = (window.location.hostname === "localhost") ? `http://localhost:27017` : `https://jf-reddit.herokuapp.com/api`;
+export const ROOT_URL = (window.location.hostname === "localhost") ? `http://localhost:3000` : `https://jf-reddit.herokuapp.com/api`;
