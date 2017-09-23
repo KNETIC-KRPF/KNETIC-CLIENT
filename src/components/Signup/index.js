717df394f63bce3bea723228e5e9363c05e73ddc
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-09-22 23:46:15 -0600

    Updated Piano Component

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Modal from 'react-modal';
 import './Signup.css';
 
-
-
-
-	render() {
-		return (
-			<div className="signup">
-				<form>
-
-				</form>
-			</div>
-		);
-	}
+const Signup = props => {
+  return (
+    <div className="signup">
+      <form></form>
+    </div>
+  );
 }
 
 export default Signup;
